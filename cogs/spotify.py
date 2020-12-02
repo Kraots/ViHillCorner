8fc5548a450b56fb3e2fa354db185304ce3e9181
Kraots1 <nagit.robertmessi@gmail.com>
2020-12-02 19:43:07 +0000

    added a check to see if the member is a bot or not, and now u can check others activity, not just urs ;)

--- cogs/spotify.py
+++ cogs/spotify.py
@@ -2,7 +2,9 @@ import discord
 from discord.ext import commands
 import utils.colors as color
 from utils.helpers import fuckmicroseconds
-from utils.helpers import time_phaser
+from discord.ext.commands import Greedy
+from discord import Member
+
 
 
 
@@ -13,21 +15,27 @@ class Spotify(commands.Cog):
         self.client = client
 
     @commands.command()
-    async def spotify(self, ctx):
+    async def spotify(self, ctx, member: discord.Member=None):
+            if member is None:
+                member = ctx.author
+            if member.bot:
+                await ctx.message.delete()
+                await ctx.send("Cannot check for spotify activity for bots! Use on members only!", delete_after=10)
+                return
         
-        if isinstance(ctx.author.activities[0],discord.activity.Spotify):
-            m =discord.Embed(title=f"{ctx.author.name} activity:")
-            m.add_field(name="Listening to :",value=ctx.author.activities[0].title, inline=False)
-            m.add_field(name="By:",value=ctx.author.activities[0].artist, inline=False)
-            m.add_field(name="On:",value =ctx.author.activities[0].album, inline=False)
-            m.add_field(name="Total Duration:",value =fuckmicroseconds(ctx.author.activities[0].duration), inline=False)
-            m.add_field(name='Link to song:', value=f"[Click Here](https://open.spotify.com/track/{ctx.author.activities[0].track_id}?si=xrjyVAxhS1y5rNHLM_WRww)", inline=False)
-            m.set_thumbnail(url=ctx.author.activities[0].album_cover_url)
-            m.color = color.inviscolor 
-            await ctx.send(embed=m)
-        else:
-            await ctx.message.delete()
-            await ctx.send("No spotify activity detected. Check if you're using a custom status, in case you do, you gotta get rid of it in order for the command to work!", delete_after=10)
+            if isinstance(member.activities[0],discord.activity.Spotify):
+                m =discord.Embed(title=f"{member.name} activity:")
+                m.add_field(name="Listening to :",value=member.activities[0].title, inline=False)
+                m.add_field(name="By:",value=member.activities[0].artist, inline=False)
+                m.add_field(name="On:",value =member.activities[0].album, inline=False)
+                m.add_field(name="Total Duration:",value =fuckmicroseconds(member.activities[0].duration), inline=False)
+                m.add_field(name='Link to song:', value=f"[Click Here](https://open.spotify.com/track/{member.activities[0].track_id}?si=xrjyVAxhS1y5rNHLM_WRww)", inline=False)
+                m.set_thumbnail(url=member.activities[0].album_cover_url)
+                m.color = color.inviscolor 
+                await ctx.send(embed=m)
+            else:
+                await ctx.message.delete()
+                await ctx.send("No spotify activity detected. Check if you're using a custom status, in case you do, you gotta get rid of it in order for the command to work!", delete_after=10)
 
 
 
