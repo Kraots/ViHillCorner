7c0e7c17039d239a5f16185a93585818ba305ebb
Kraots1 <nagit.robertmessi@gmail.com>
2020-12-06 11:06:01 +0000

    finally made it :3

--- cogs/spotify.py
+++ cogs/spotify.py
@@ -1,7 +1,8 @@
 import discord
 from discord.ext import commands
-
-
+import datetime as dt
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 
 class Spotify(commands.Cog):
    
@@ -24,12 +25,16 @@ class Spotify(commands.Cog):
                 return
         
             if isinstance(member.activities[1], discord.activity.Spotify):
+                diff = relativedelta(datetime.utcnow(), member.activities[1].created_at)
+                
                 m =discord.Embed(title=f"{member.name} activity:")
                 m.add_field(name="Listening to :",value=member.activities[1].title, inline=False)
                 m.add_field(name="By:",value=member.activities[1].artist, inline=False)
                 m.add_field(name="On:",value =member.activities[1].album, inline=False)
                 m2, s2 = divmod(int(member.activities[1].duration.seconds), 60)
-                song_length = f'{m2}:{s2}'
+                song_length = '{:02}:{:02}'.format(m2, s2)
+                playingfor = '{:02}:{:02}'.format(diff.minutes, diff.seconds)
+                m.add_field(name="Duration:", value=f"{playingfor} - {song_length}")
                 m.add_field(name="Total Duration:",value =song_length, inline=False)
                 m.add_field(name='Link to song:', value=f"[Click Here](https://open.spotify.com/track/{member.activities[1].track_id}?si=xrjyVAxhS1y5rNHLM_WRww)", inline=False)
                 m.set_thumbnail(url=member.activities[1].album_cover_url)
@@ -44,12 +49,15 @@ class Spotify(commands.Cog):
             member = ctx.author
         if isinstance(error, commands.errors.CommandInvokeError):
             if isinstance(member.activities[0],discord.activity.Spotify):
+                diff = relativedelta(datetime.utcnow(), member.activities[0].created_at)
                 m =discord.Embed(title=f"{member.name} activity:")
                 m.add_field(name="Listening to :",value=member.activities[0].title, inline=False)
                 m.add_field(name="By:",value=member.activities[0].artist, inline=False)
                 m.add_field(name="On:",value =member.activities[0].album, inline=False)
                 m1, s1 = divmod(int(member.activities[0].duration.seconds), 60)
-                song_length = f'{m1}:{s1}'
+                song_length = '{:02}:{:02}'.format(m1, s1)
+                playingfor = '{:02}:{:02}'.format(diff.minutes, diff.seconds)
+                m.add_field(name="Duration:", value=f"{playingfor} - {song_length}")
                 m.add_field(name="Total Duration:",value =song_length, inline=False)
                 m.add_field(name='Link to song:', value=f"[Click Here](https://open.spotify.com/track/{member.activities[0].track_id}?si=xrjyVAxhS1y5rNHLM_WRww)", inline=False)
                 m.set_thumbnail(url=member.activities[0].album_cover_url)
