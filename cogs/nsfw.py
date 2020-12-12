52441ec25cf3c69a279f28d9fa8bcbf47ce6baf0
Kraots1 <nagit.robertmessi@gmail.com>
2020-12-12 19:32:28 +0000

    .

--- cogs/nsfw.py
+++ cogs/nsfw.py
@@ -106,10 +106,10 @@ class NSFW(commands.Cog):
 
 
     @nsfw.error
-    async def suggest_error(self, ctx, error):
+    async def nsfw_error(self, ctx, error):
         if isinstance(error, commands.CheckFailure):
             if "Staff" in [role.name for role in ctx.message.author.roles]:
-                await ctx.send('Invalid format!\nUse: `.nsfw add {user}` or `.nsfw remove {user}`!')
+                await ctx.send('Invalid format!\nUse: `!nsfw add {user}` or `!nsfw remove {user}`!')
             else:
 
                 msg = f"This command is only usable in a nsfw marked channel!\n_ _ _ _ _ _ _ _ _ _ _ _ _ _ {ctx.author.mention}"
