7cf0c959aa4183c107b04388d10d1ffc37d68266
Kraots1 <nagit.robertmessi@gmail.com>
2020-12-22 19:15:41 +0000

    fix

--- cogs/caps-filter.py
+++ cogs/caps-filter.py
@@ -23,58 +23,63 @@ class CapsFilter(commands.Cog):
 		if message.guild:
 			check_this = message.content
 
-			result = sum(1 for x in check_this if x.isupper())
+			if message.content.lower().startswith("https://"):
+				return
 
-			if result > 5:
-				await message.delete()
+			else:
 
-				await open_warns(user)
-				users = await get_warns_data()
+				result = sum(1 for x in check_this if x.isupper())
 
-				users[str(user.id)]["warns"] += 1
+				if result > 5:
+					await message.delete()
 
-				with open("caps-warns.json", "w") as f:
-					json.dump(users, f)
-				
-				total_warns = users[str(user.id)]["warns"]
+					await open_warns(user)
+					users = await get_warns_data()
 
-				if total_warns > 1:
+					users[str(user.id)]["warns"] += 1
 
-					del users[str(user.id)]
 					with open("caps-warns.json", "w") as f:
 						json.dump(users, f)
+					
+					total_warns = users[str(user.id)]["warns"]
 
-					if "Staff" in [role.name for role in user.roles]:
-						await user.remove_roles(staff, mod)
-						await user.add_roles(muted)
-						msg1 = "You have been muted in `ViHill Corner`."
-						em1 = discord.Embed(description=f"**Reason:** [Caps]({message.jump_url})")
-						await user.send(msg1, embed=em1)
-						msg2 = f"**{user}** has been muted."
-						em2 = discord.Embed(description=f"**Reason:** [Caps]({message.jump_url})")
-						await message.channel.send(msg2, embed=em2)
-						await asyncio.sleep(720)
-						await user.remove_roles(muted)
-						await user.add_roles(staff, mod)
-						await user.send("You have been unmuted in `ViHill Corner`.")
-				
+					if total_warns > 1:
+
+						del users[str(user.id)]
+						with open("caps-warns.json", "w") as f:
+							json.dump(users, f)
+
+						if "Staff" in [role.name for role in user.roles]:
+							await user.remove_roles(staff, mod)
+							await user.add_roles(muted)
+							msg1 = "You have been muted in `ViHill Corner`."
+							em1 = discord.Embed(description=f"**Reason:** [Caps]({message.jump_url})")
+							await user.send(msg1, embed=em1)
+							msg2 = f"**{user}** has been muted."
+							em2 = discord.Embed(description=f"**Reason:** [Caps]({message.jump_url})")
+							await message.channel.send(msg2, embed=em2)
+							await asyncio.sleep(720)
+							await user.remove_roles(muted)
+							await user.add_roles(staff, mod)
+							await user.send("You have been unmuted in `ViHill Corner`.")
+					
+						else:
+							await user.add_roles(muted)
+							msg1 = "You have been muted in `ViHill Corner`."
+							em1 = discord.Embed(description=f"**Reason:** [Caps]({message.jump_url})")
+							await user.send(msg1, embed=em1)
+							msg2 = f"**{user}** has been muted."
+							em2 = discord.Embed(description=f"**Reason:** [Caps]({message.jump_url})")
+							await message.channel.send(msg2, embed=em2)
+							await asyncio.sleep(720)
+							await user.remove_roles(muted)
+							await user.send("You have been unmuted in `ViHill Corner`.")
+
 					else:
-						await user.add_roles(muted)
-						msg1 = "You have been muted in `ViHill Corner`."
-						em1 = discord.Embed(description=f"**Reason:** [Caps]({message.jump_url})")
-						await user.send(msg1, embed=em1)
-						msg2 = f"**{user}** has been muted."
-						em2 = discord.Embed(description=f"**Reason:** [Caps]({message.jump_url})")
-						await message.channel.send(msg2, embed=em2)
-						await asyncio.sleep(720)
-						await user.remove_roles(muted)
-						await user.send("You have been unmuted in `ViHill Corner`.")
-
+						return
 				else:
 					return
-			else:
-				return
-		
+			
 
 
 async def open_warns(user):
