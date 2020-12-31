12e1cb032198585ceee8b4028cb67ca1b0003cba
Kraots1 <nagit.robertmessi@gmail.com>
2020-12-31 11:14:57 +0000

    .

--- cogs/cat.py
+++ cogs/cat.py
@@ -5,26 +5,27 @@ import utils.colors as color
 
 class Cat(commands.Cog):
 
-    def __init__(self, client):
-        self.client = client
-        self.prefix = "!"
-    async def cog_check(self, ctx):
-        return ctx.prefix == self.prefix
+	def __init__(self, client):
+		self.client = client
+		self.prefix = "!"
+	async def cog_check(self, ctx):
+		return ctx.prefix == self.prefix
 
-    @commands.command()
-    async def cat(self, ctx):
-        async with aiohttp.ClientSession() as cs:
-          async with cs.get("http://aws.random.cat/meow") as r:
-            data = await r.json()
+	@commands.command()
+	async def cat(self, ctx):
+		async with aiohttp.ClientSession() as cs:
+			async with cs.get("http://aws.random.cat/meow") as r:
+				data = await r.json()
 
-          imgUrl = data['file']
+			imgUrl = data['file']
 
-          embed = discord.Embed(title="Cat", url=imgUrl, color=color.orange, timestamp=ctx.message.created_at)
-          embed.set_image(url=imgUrl)
-          embed.set_footer(text=f'Requested by: {ctx.author}', icon_url=ctx.author.avatar_url)
-          await ctx.channel.send(embed=embed)
-
+			embed = discord.Embed(title="Cat", url=imgUrl, color=color.orange, timestamp=ctx.message.created_at)
+			embed.set_image(url=imgUrl)
+			embed.set_footer(text=f'Requested by: {ctx.author}', icon_url=ctx.author.avatar_url)
+			msg = await ctx.channel.send(embed=embed)
+			await msg.add_reaction("❤️")
+			await msg.add_reaction("😸")
 
 
 def setup(client):
-    client.add_cog(Cat(client))
+	client.add_cog(Cat(client))
--- cogs/dog.py
+++ cogs/dog.py
@@ -5,23 +5,25 @@ import utils.colors as color
 
 class Dog(commands.Cog):
 
-    def __init__(self, client):
-        self.client = client
-        self.prefix = "!"
-    async def cog_check(self, ctx):
-        return ctx.prefix == self.prefix
+	def __init__(self, client):
+		self.client = client
+		self.prefix = "!"
+	async def cog_check(self, ctx):
+		return ctx.prefix == self.prefix
 
 
-    @commands.command()
-    async def dog(self, ctx): 
-        async with aiohttp.ClientSession() as cs:
-          async with cs.get("http://random.dog/woof.json") as r:
-            data = await r.json()
+	@commands.command()
+	async def dog(self, ctx): 
+		async with aiohttp.ClientSession() as cs:
+			async with cs.get("http://random.dog/woof.json") as r:
+				data = await r.json()
 
-            embed = discord.Embed(title="Dog", url=data['url'], color=color.orange, timestamp=ctx.message.created_at)
-            embed.set_image(url=data['url'])
-            embed.set_footer(text=f'Requested by: {ctx.author}', icon_url=ctx.author.avatar_url)
-            await ctx.channel.send(embed=embed)
+			embed = discord.Embed(title="Dog", url=data['url'], color=color.orange, timestamp=ctx.message.created_at)
+			embed.set_image(url=data['url'])
+			embed.set_footer(text=f'Requested by: {ctx.author}', icon_url=ctx.author.avatar_url)
+			msg = await ctx.channel.send(embed=embed)
+			await msg.add_reaction("❤️")
+			await msg.add_reaction("🐶")
 
 def setup(client):
-    client.add_cog(Dog(client))
+	client.add_cog(Dog(client))
