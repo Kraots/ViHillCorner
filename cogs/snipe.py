1756acc519f6bfa40ab65d768e11f8881592869d
Kraots1 <nagit.robertmessi@gmail.com>
2021-01-03 12:32:51 +0000

    last quick fix

--- cogs/snipe.py
+++ cogs/snipe.py
@@ -5,42 +5,38 @@ snipes = {}
 
 class Snipe(commands.Cog):
 
-    def __init__(self, client):
-        self.client = client
-        self.prefix = "!"
-    async def cog_check(self, ctx):
-        return ctx.prefix == self.prefix
+	def __init__(self, client):
+		self.client = client
+		self.prefix = "!"
+	async def cog_check(self, ctx):
+		return ctx.prefix == self.prefix
 
 
 
-    @commands.Cog.listener()
-    async def on_message_delete(self, message: discord.Message):
-        snipes[message.channel.id] = message
+	@commands.Cog.listener()
+	async def on_message_delete(self, message: discord.Message):
+		if message.author.id == 374622847672254466:
+			return
+		elif message.author.bot:
+			return
+		else:
+			snipes[message.channel.id] = message
 
-    
-    @commands.command()
-    async def snipe(self, ctx, *, channel: discord.TextChannel = None):
-        channel = channel or ctx.channel
-        try:
-            msg = snipes[channel.id]
-        except KeyError:
-            return await ctx.send('Nothing to snipe!', delete_after=5)
-        
-        if msg.author.id == 374622847672254466:
-            await ctx.send('Hah! You tried, but no ;))')
-            return
+	
+	@commands.command()
+	async def snipe(self, ctx, *, channel: discord.TextChannel = None):
+		channel = channel or ctx.channel
+		try:
+			msg = snipes[channel.id]
+		except KeyError:
+			return await ctx.send('Nothing to snipe!', delete_after=5)
 
-        if msg.author.bot:
-            await ctx.send(f'Cannot snipe messages sent by bots!\n_ _ _ _ Bot: {msg.author.mention}')
-            return
-
-        else:
-            embed = discord.Embed(description= msg.content, color=msg.author.color, timestamp=msg.created_at)
-            embed.set_author(name=msg.author, icon_url=msg.author.avatar_url)
-            await ctx.send(embed=embed)
+		embed = discord.Embed(description= msg.content, color=msg.author.color, timestamp=msg.created_at)
+		embed.set_author(name=msg.author, icon_url=msg.author.avatar_url)
+		await ctx.send(embed=embed)
 
 
-            
+			
 
 def setup (client):
-    client.add_cog(Snipe(client))
+	client.add_cog(Snipe(client))
