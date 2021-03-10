1baae6f89c62bf9300f440170a588fc19c81b134
Kraots1 <nagit.robertmessi@gmail.com>
2021-03-10 15:22:16 +0000

    should fix name/nickname filter hopefully :thinking:

--- cogs/nick-filter.py
+++ cogs/nick-filter.py
@@ -7,11 +7,9 @@ import re
 def remove_emoji(string):
     emoji_pattern = re.compile("["
                            u"\U0001F600-\U0001F64F"  # emoticons
-                           u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                           u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                           u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           u"\U0001F300-\U0001F251"  # symbols & pictographs
+                           u"\U0001F680"  # transport & map symbols
                            u"\U00002702-\U000027B0"
-                           u"\U000024C2-\U0001F251"
                            "]+", flags=re.UNICODE)
     return emoji_pattern.sub(r'', string)
 
