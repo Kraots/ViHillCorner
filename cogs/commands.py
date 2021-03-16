b1fde367232a50864e0d4aae5f4e67206d0e3e83
Kraots1 <nagit.robertmessi@gmail.com>
2021-03-16 18:44:49 +0000

    changed ppsuck :weary:

--- cogs/commands.py
+++ cogs/commands.py
@@ -105,11 +105,8 @@ class command(commands.Cog):
 		if member is None:
 			member = ctx.author
 
-		if member.id in [374622847672254466] and not ctx.author.id == [374622847672254466]:
+		if member.id in [374622847672254466, 653611679082348544] and not ctx.author.id == [374622847672254466, 653611679082348544]:
 			return
-		
-		#elif member.id == <ID> and not ctx.author.id in [<ID>, 374622847672254466]:
-		#	return
 			
 		avatar = discord.Embed(title=f"{member.name}", url=f"{member.avatar_url}", color=color.blue)
 		avatar.set_image(url=member.avatar_url)
