5a09b3e8492cae1fefc2757df6b439ec0af7fda5
Kraots <nagit.robertmessi@gmail.com>
2021-06-18 19:34:47 +0300

    • Improved Calc Command

--- cogs/calc.py
+++ cogs/calc.py
@@ -1,12 +1,47 @@
 import discord
 from discord.ext import commands
-import wolframalpha
 import utils.colors as color
 import os
+import re
+NUMBER_REGEX = r"[0-9\.]+"
 
-wolf_key = os.getenv("WOLFRAM_KEY")
+class Calculatorr:
+	operators = ("^", "/", "*", "-", "+")    
 
-wolf = wolframalpha.Client(wolf_key)
+	def __init__(self, expression: str):
+		self.expression = expression.lower().replace(" ", "").strip()
+
+	def __repr__(self):
+		def recur(op):
+			self.sub_regex(op)
+			if re.search(fr"(-?{NUMBER_REGEX}\{op}-?{NUMBER_REGEX})", self.expression):
+				recur(op)
+		for op in self.operators:
+			if re.search(fr"(-?{NUMBER_REGEX}\{op}-?{NUMBER_REGEX})", self.expression):
+				recur(op)
+		return self.expression
+
+	def append(self, obj):
+		self.expression += str(obj.lower().replace(" ", "").strip())
+
+	def sub_regex(self, operator):
+		def sub_fn(v):
+			x, y = v.group().split(operator)
+			x, y = float(x), float(y)
+			conv = {
+				"/": x/y,
+				"*": x*y,
+				"-": x-y,
+				"+": x+y,
+				"^": x**y
+			}
+			return str(conv.get(operator))
+		try:
+			self.expression = re.sub(fr"(-?{NUMBER_REGEX}\{operator}-?{NUMBER_REGEX})", sub_fn, self.expression)
+		except TypeError:
+			pass
+		except OverflowError:
+			self.expression = "This operation is not possible."
 
 class Calculator(commands.Cog):
 
@@ -17,26 +52,16 @@ class Calculator(commands.Cog):
 		return ctx.prefix == self.prefix
 
 	@commands.command(aliases=['calculator', 'calculate'])
-	async def calc(self, ctx, *args):
-		async with ctx.typing():
-			query = " ".join(args)
-			res = wolf.query(query)
-			try:
-				result = next(res.results).text
-			except Exception:
-				await ctx.send("Failed.")
-				return
-
-			em = discord.Embed(color=color.lightpink, title="Calculator")
-			em.add_field(name="Operation:", value=query, inline=False)
-			em.add_field(name="Result:", value=result, inline=False)
-			em.set_footer(text=f"Requested by: {ctx.author}", icon_url=ctx.author.avatar_url)
-			
-			await ctx.send(embed=em)
-			return
-
-
-
+	async def calc(self, ctx, *, args:str):
+		result = Calculatorr(args)
+		if str(result).endswith('.0'):
+			result = str(result).replace('.0', '')
+		em = discord.Embed(color=color.lightpink, title="Calculator")
+		em.add_field(name="Operation:", value=args, inline=False)
+		em.add_field(name="Result:", value=result, inline=False)
+		em.set_footer(text=f"Requested by: {ctx.author}", icon_url=ctx.author.avatar_url)
+		
+		await ctx.send(embed=em)
 
 
 
