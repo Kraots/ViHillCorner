1088fb7b78fd714817deae7d67b25d65d9f01706
Kraots <nagit.robertmessi@gmail.com>
2021-06-20 21:08:03 +0300

    • Replaced `!define` and `!search` with `!google`

--- cogs/help.py
+++ cogs/help.py
@@ -18,7 +18,7 @@ class Help(commands.Cog):
 		helpEm = discord.Embed(description="To get help for a certain command type `!help <command_name>`", color=color.lightpink)
 		helpEm.set_footer(text='To get help for a certain command type `!help <command_name>`\n', icon_url=ctx.author.avatar_url)
 		helpEm.set_thumbnail(url=self.client.user.avatar_url)
-		helpEm.add_field(name="Commands", value="`ee`, `nick`, `profile`, `created`, `joined`, `av`, `waifu`, `invite`, `suggest`, `spotify`, `meme`, `cat`, `dog`, `snipe`, `nsfw`, `topic`, `gayrate`, `straightrate` , `simprate`, `hornyrate`, `boomerrate`, `8ball`, `fight`, `ppsize`, `birthday`, `intro`, `whois`, `reclist`, `dev-portal`, `perm-calc`, `cr`, `vampify`, `clapify`, `define`, `search`, `calculator`, `marry`, `marriedwho`, `divorce`, `scrs`, `tag`, `snippets`, `role-id`, `rank`, `remind`, `top`, `bdsm`, `randomnumber`, `multiplier`, `trivia`")
+		helpEm.add_field(name="Commands", value="`ee`, `nick`, `profile`, `created`, `joined`, `av`, `waifu`, `invite`, `suggest`, `spotify`, `meme`, `cat`, `dog`, `snipe`, `nsfw`, `topic`, `gayrate`, `straightrate` , `simprate`, `hornyrate`, `boomerrate`, `8ball`, `fight`, `ppsize`, `birthday`, `intro`, `whois`, `reclist`, `dev-portal`, `perm-calc`, `cr`, `vampify`, `clapify`, `google`, `calculator`, `marry`, `marriedwho`, `divorce`, `scrs`, `tag`, `snippets`, `role-id`, `rank`, `remind`, `top`, `bdsm`, `randomnumber`, `multiplier`, `trivia`")
 		helpEm.add_field(name="Economy", value="`register`, `unregister` ,`balance`, `deposit`, `withdraw`, `steal`, `slots`, `beg`, `give`, `work`, `crime`, `guess`, `race`, `ppsuck`, `daily`, `rock-paper-scissors`", inline=False)
 		helpEm.add_field(name="Info", value="`untill-partner`, `membercount`, `sfw`, `spam`, `english`, `botinfo`, `uptime`, `ping`, `serverad`, `rawad`, `serverinfo`, `vote`, `update`", inline=False)
 		if "Staff" in [role.name for role in ctx.message.author.roles]:
@@ -167,15 +167,9 @@ class Help(commands.Cog):
 		await ctx.send(embed=em)
 
 	@help.command()
-	async def search(self, ctx):
-		em = discord.Embed(color=color.lightpink, title="***Usage:***", description="```CSS\n!define <query>```")
-		em.add_field(name="***Info:***", value="• Search for a query.")
-		await ctx.send(embed=em)
-
-	@help.command()
-	async def define(self, ctx):
-		em = discord.Embed(color=color.lightpink, title="***Usage:***", description="```CSS\n!define <word>```")
-		em.add_field(name="***Info:***", value="• Search the definition of a word.")
+	async def google(self, ctx):
+		em = discord.Embed(color=color.lightpink, title="***Usage:***", description="```CSS\n!google <query>```")
+		em.add_field(name="***Info:***", value="• Search for something on google.")
 		await ctx.send(embed=em)
 
 	@help.command(aliases=['snippets'])
