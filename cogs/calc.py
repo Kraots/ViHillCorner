6688191176da82d4ce5ee6f79616f3ffda716821
Kraots <nagit.robertmessi@gmail.com>
2021-06-29 19:21:48 +0300

    • Improved `!calc` command

--- cogs/calc.py
+++ cogs/calc.py
@@ -1,4 +1,5 @@
 import discord
+from discord.errors import HTTPException
 from discord.ext import commands
 import utils.colors as color
 import async_tio
@@ -19,13 +20,24 @@ class Calculator(commands.Cog):
 		tio = await async_tio.Tio()
 		result = await tio.execute(f"print({args})", language="python3")
 		await tio.close()
+		exit_status_ = result.exit_status
+		if int(exit_status_) == 1:
+			return await ctx.reply("Are you sure you're using this command for a valid operation?")
 
-		em = discord.Embed()
-		em.set_author(name=f"Here's your result `{ctx.author.display_name}`:", icon_url=ctx.author.avatar_url)
-		em.add_field(name="Operation:", value=args, inline=False)
-		em.add_field(name="Result:", value=result.stdout)
-		em.color = color.lightpink
-		await ctx.send(embed=em)
+		try:
+			em = discord.Embed()
+			em.set_author(name=f"Here's your result `{ctx.author.display_name}`:", icon_url=ctx.author.avatar_url)
+			em.add_field(name="Operation:", value=f"`{args}`", inline=False)
+			em.add_field(name="Result:", value=f"`{result.stdout}`")
+			em.color = color.lightpink
+			await ctx.send(embed=em, reference=ctx.replied_reference)
+		except HTTPException:
+			em = discord.Embed()
+			em.set_author(name=f"Here's your result `{ctx.author.display_name}`:", icon_url=ctx.author.avatar_url)
+			em.add_field(name="Operation:", value="`Operation too long`", inline=False)
+			em.add_field(name="Result:", value=f"`{result.stdout}`")
+			em.color = color.lightpink
+			await ctx.send(embed=em, reference=ctx.replied_reference)
 
 
 
