edd408481abcf319c9ddc12ebc050a44d357e453
Kraots <nagit.robertmessi@gmail.com>
2021-07-06 20:33:26 +0300

    • [Bug Fixes] • [Improvements]

--- cogs/rr.py
+++ cogs/rr.py
@@ -14,6 +14,20 @@ class ReactionRoles(commands.Cog):
 		COLORSMSG2 = 779389533875601451
 		COLORSMSG3 = 788117110475718728
 
+		# ------------------------------------
+
+		if payload.message_id == 862009256421359657:
+			if payload.member.id != 374622847672254466:
+				if str(payload.emoji) in ['<a:kiss1:761219243232067615>', '<a:kiss2:761219249896816661>', '<a:hearthug:759685245422927892>']:
+					ch = guild.get_channel(payload.channel_id)
+					msg = await ch.fetch_message(862009256421359657)
+					await msg.remove_reaction(payload.emoji, payload.member)
+					await payload.member.send("You cannot add this type of reaction you scum. That person is taken and only carrots can add them :rage:\nBtw this has been sent to me so be prepared to die :smiley: :knife:")
+					kraots = guild.get_member(374622847672254466)
+					await kraots.send(f"`{payload.member}` added the reaction {payload.emoji} to your bbg's intro master, punish them :smiling_imp:")
+
+		# ------------------------------------
+
 
 		message_id = payload.message_id
 		member = payload.member
@@ -73,7 +87,7 @@ class ReactionRoles(commands.Cog):
 				return
 
 
-			if message_id == COLORSMSG2:
+			elif message_id == COLORSMSG2:
 				the_emoji = payload.emoji.name
 				role = msg2_emojis[the_emoji]
 				final_role = guild.get_role(role)
@@ -81,7 +95,7 @@ class ReactionRoles(commands.Cog):
 				await member.edit(roles=Roles)
 				return
 
-			if message_id == COLORSMSG3:
+			elif message_id == COLORSMSG3:
 				the_emoji = payload.emoji.name
 				role = msg3_emojis[the_emoji]
 				final_role = guild.get_role(role)
