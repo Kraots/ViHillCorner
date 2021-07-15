7ec540453a2aa53abe0852262d06b6f9c5a1eb42
Kraots <nagit.robertmessi@gmail.com>
2021-07-15 11:01:55 +0300

    • Removed and improved a lot of the old code.

--- cogs/serverinfo.py
+++ cogs/serverinfo.py
@@ -12,64 +12,46 @@ class ServerInfo(commands.Cog):
 		return ctx.prefix == self.prefix
 
 
-	@commands.group(aliases=['server', 'sinfo', 'si'], pass_context=True, invoke_without_command=True)
-	async def serverinfo(self, ctx, *, msg=""):
+	@commands.command(aliases=['server', 'sinfo', 'si'])
+	async def serverinfo(self, ctx):
 		await ctx.message.delete()
-		if ctx.invoked_subcommand is None:
-			if msg:
-				server = None
-				try:
-					float(msg)
-					server = self.client.get_guild(int(msg))
-					if not server:
-						return await ctx.send(
-											self.client.client_prefix + 'Server not found.')
-				except:
-					for i in self.client.guilds:
-						if i.name.lower() == msg.lower():
-							server = i
-							break
-					if not server:
-						return await ctx.send(self.client.client_prefix + 'Could not find server. Note: You must be a member of the server you are trying to search.')
-			else:
-				server = ctx.message.guild
+		guild = self.client.get_guild(750160850077089853)
+		online = 0
+		for i in guild.members:
+			if str(i.status) == 'online' or str(i.status) == 'idle' or str(i.status) == 'dnd':
+				online += 1
+		all_users = []
+		for user in guild.members:
+			all_users.append('{0.name}#{0.discriminator}'.format(user))
+		all_users.sort()
 
-			online = 0
-			for i in server.members:
-				if str(i.status) == 'online' or str(i.status) == 'idle' or str(i.status) == 'dnd':
-					online += 1
-			all_users = []
-			for user in server.members:
-				all_users.append('{}#{}'.format(user.name, user.discriminator))
-			all_users.sort()
+		channel_count = len([x for x in guild.channels if type(x) == discord.channel.TextChannel])
 
-			channel_count = len([x for x in server.channels if type(x) == discord.channel.TextChannel])
+		role_count = len(guild.roles)
+		emoji_count = len(guild.emojis)
 
-			role_count = len(server.roles)
-			emoji_count = len(server.emojis)
+		def format_date(dt):
+			if dt is None:
+				return 'N/A'
+			return f'{dt:%Y-%m-%d %H:%M} ({time.human_timedelta(dt, accuracy=3)})'
 
-			def format_date(dt):
-				if dt is None:
-					return 'N/A'
-				return f'{dt:%Y-%m-%d %H:%M} ({time.human_timedelta(dt, accuracy=3)})'
-
-			em = discord.Embed(color=color.lightpink)
-			em.add_field(name='Name | ID', value=f"{server.name}  |  {server.id}")
-			em.add_field(name='Owner', value=server.owner, inline=False)
-			em.add_field(name='Members', value=server.member_count)
-			em.add_field(name='Currently Online', value=online)
-			em.add_field(name='Text Channels', value=str(channel_count))
-			em.add_field(name='Region', value=server.region)
-			em.add_field(name='Verification Level', value=str(server.verification_level))
-			em.add_field(name='Highest role', value="Staff")
-			em.add_field(name='Number of roles', value=str(role_count))
-			em.add_field(name='Number of emotes', value=str(emoji_count))
-			em.add_field(name='Created At', value=format_date(server.created_at))
-			em.set_thumbnail(url=server.icon_url)
-			em.set_author(name='Server Info')
-			em.set_footer(text=f'Requested by: {ctx.author}', icon_url=ctx.author.avatar_url)
-			
-			await ctx.channel.send(embed=em)
+		em = discord.Embed(color=color.lightpink)
+		em.add_field(name='Name | ID', value=f"{guild.name}  |  {guild.id}")
+		em.add_field(name='Owner', value=guild.owner, inline=False)
+		em.add_field(name='Users', value=f"{len([m for m in guild.members if not m.bot])} members | {len([m for m in guild.members if m.bot])} bots")
+		em.add_field(name='Currently Online', value=online)
+		em.add_field(name='Text Channels', value=str(channel_count))
+		em.add_field(name='Region', value=guild.region)
+		em.add_field(name='Verification Level', value=str(guild.verification_level))
+		em.add_field(name='Highest role', value="Staff")
+		em.add_field(name='Number of roles', value=str(role_count))
+		em.add_field(name='Number of emotes', value=str(emoji_count))
+		em.add_field(name='Created At', value=format_date(guild.created_at))
+		em.set_thumbnail(url=guild.icon_url)
+		em.set_author(name='Server Info')
+		em.set_footer(text=f'Requested by: {ctx.author}', icon_url=ctx.author.avatar_url)
+		
+		await ctx.send(embed=em, reference=ctx.replied_reference)
 
 def setup (client):
 	client.add_cog(ServerInfo(client))
