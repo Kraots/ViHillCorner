28e590fc4c57ac052c085633eb29f4f8d2d79de3
Kraots <70799557+Kraots@users.noreply.github.com>
2021-09-09 00:24:22 +0300

    Using new discord buttons for every reaction based paginator

--- utils/paginator.py
+++ utils/paginator.py
@@ -64,148 +64,6 @@ class RoboPages(menus.MenuPages):
 		except Exception:
 			pass
 
-class FieldPageSource(menus.ListPageSource):
-	"""A page source that requires (field_name, field_value) tuple items."""
-	def __init__(self, entries, *, per_page=12):
-		super().__init__(entries, per_page=per_page)
-		self.embed = disnake.Embed(colour=disnake.Colour.blurple())
-
-	async def format_page(self, menu, entries):
-		self.embed.clear_fields()
-		self.embed.description = disnake.Embed.Empty
-
-		for key, value in entries:
-			self.embed.add_field(name=key, value=value, inline=False)
-
-		maximum = self.get_max_pages()
-		if maximum > 1:
-			text = f'Page {menu.current_page + 1}/{maximum} ({len(self.entries)} entries)'
-			self.embed.set_footer(text=text)
-
-		return self.embed
-
-class TextPageSource(menus.ListPageSource):
-	def __init__(self, text, *, prefix='```', suffix='```', max_size=2000):
-		pages = CommandPaginator(prefix=prefix, suffix=suffix, max_size=max_size - 200)
-		for line in text.split('\n'):
-			pages.add_line(line)
-
-		super().__init__(entries=pages, per_page=1)
-
-	async def format_page(self, menu, content):
-		maximum = self.get_max_pages()
-		if maximum > 1:
-			return f'{content}\nPage {menu.current_page + 1}/{maximum}'
-		return content
-
-class SimplePageSource(menus.ListPageSource):
-	def __init__(self, entries, *, per_page=12):
-		super().__init__(entries, per_page=per_page)
-		self.initial_page = True
-
-	async def format_page(self, menu, entries):
-		pages = []
-		for index, entry in enumerate(entries, start=menu.current_page * self.per_page):
-			pages.append(f'{index + 1}. {entry}')
-
-		maximum = self.get_max_pages()
-		if maximum > 1:
-			footer = f'Page {menu.current_page + 1}/{maximum} ({len(self.entries)} entries)'
-			menu.embed.set_footer(text=footer)
-
-		if self.initial_page and self.is_paginating():
-			pages.append('')
-			pages.append('Confused? React with \N{INFORMATION SOURCE} for more info.')
-			self.initial_page = False
-
-		menu.embed.description = '\n'.join(pages)
-		return menu.embed
-
-class SimplePages(RoboPages):
-	"""A simple pagination session reminiscent of the old Pages interface.
-
-	Basically an embed with some normal formatting.
-	"""
-
-	def __init__(self, entries, *, per_page=12, color=None):
-		super().__init__(SimplePageSource(entries, per_page=per_page))
-		if color == None:
-			color = disnake.Color.blurple()
-		self.embed = disnake.Embed(colour=color)
-
-
-
-
-class CustomRobo(menus.MenuPages):
-	def __init__(self, source):
-		super().__init__(source=source, check_embeds=True)
-
-	async def finalize(self, timed_out):
-		try:
-			if timed_out:
-				await self.message.clear_reactions()
-			else:
-				await self.message.delete()
-		except disnake.HTTPException:
-			pass
-
-class NewHelpMenus(menus.ListPageSource):
-	def __init__(self, entries, *, per_page=12):
-		super().__init__(entries, per_page=per_page)
-		
-	async def format_page(self, menu, entries):
-		pages = []
-		for index, entry in enumerate(entries, start=menu.current_page * self.per_page):
-			pages.append(f'{entry}')
-		
-		maximum = self.get_max_pages()
-		if maximum > 1:
-			footer = f'Page {menu.current_page + 1}/{maximum} ({len(self.entries)} commands)'
-			menu.embed.set_footer(text=footer)
-	
-		menu.embed.description = "\n".join(pages)
-		return menu.embed
-
-class NewCustomMenus(menus.ListPageSource):
-	def __init__(self, entries, *, per_page=12):
-		super().__init__(entries, per_page=per_page)
-		
-	async def format_page(self, menu, entries):
-		pages = []
-		for index, entry in enumerate(entries, start=menu.current_page * self.per_page):
-			pages.append(f'`{index + 1}.` {entry}')
-
-		maximum = self.get_max_pages()
-		if maximum > 1:
-			footer = f'Page {menu.current_page + 1}/{maximum} ({len(self.entries)} entries)'
-			menu.embed.set_footer(text=footer)
-	
-		menu.embed.description = "\n".join(pages)
-		return menu.embed
-
-class HelpmMenu(CustomRobo):
-	def __init__(self, entries, *, per_page=12, title="", color=None):
-		super().__init__(NewHelpMenus(entries, per_page=per_page))
-		if color == None:
-			color = disnake.Color.blurple()
-		self.embed = disnake.Embed(colour=color, title=title)
-
-class CustomMenu(CustomRobo):
-	def __init__(self, entries, *, per_page=12, title="", color=None):
-		super().__init__(NewCustomMenus(entries, per_page=per_page))
-		if color == None:
-			color = disnake.Color.blurple()
-		self.embed = disnake.Embed(colour=color, title=title)
-
-class ToDoMenu(CustomRobo):
-	def __init__(self, entries, *, per_page=12, title="", color=None, author_name=None, author_icon_url=None):
-		super().__init__(NewHelpMenus(entries, per_page=per_page))
-		if color == None:
-			color = disnake.Color.blurple()
-		self.embed = disnake.Embed(colour=color, title=title)
-		if author_name != None:
-			self.embed.set_author(name=author_name, url=author_icon_url, icon_url=author_icon_url)
-
 class _RoboPages(disnake.ui.View):
 	def __init__(
 		self,
@@ -394,3 +252,145 @@ class _RoboPages(disnake.ui.View):
 		await interaction.response.defer()
 		await interaction.delete_original_message()
 		self.stop()
+
+class FieldPageSource(menus.ListPageSource):
+	"""A page source that requires (field_name, field_value) tuple items."""
+	def __init__(self, entries, *, per_page=12):
+		super().__init__(entries, per_page=per_page)
+		self.embed = disnake.Embed(colour=disnake.Colour.blurple())
+
+	async def format_page(self, menu, entries):
+		self.embed.clear_fields()
+		self.embed.description = disnake.Embed.Empty
+
+		for key, value in entries:
+			self.embed.add_field(name=key, value=value, inline=False)
+
+		maximum = self.get_max_pages()
+		if maximum > 1:
+			text = f'Page {menu.current_page + 1}/{maximum} ({len(self.entries)} entries)'
+			self.embed.set_footer(text=text)
+
+		return self.embed
+
+class TextPageSource(menus.ListPageSource):
+	def __init__(self, text, *, prefix='```', suffix='```', max_size=2000):
+		pages = CommandPaginator(prefix=prefix, suffix=suffix, max_size=max_size - 200)
+		for line in text.split('\n'):
+			pages.add_line(line)
+
+		super().__init__(entries=pages, per_page=1)
+
+	async def format_page(self, menu, content):
+		maximum = self.get_max_pages()
+		if maximum > 1:
+			return f'{content}\nPage {menu.current_page + 1}/{maximum}'
+		return content
+
+class SimplePageSource(menus.ListPageSource):
+	def __init__(self, entries, *, per_page=12):
+		super().__init__(entries, per_page=per_page)
+		self.initial_page = True
+
+	async def format_page(self, menu, entries):
+		pages = []
+		for index, entry in enumerate(entries, start=menu.current_page * self.per_page):
+			pages.append(f'{index + 1}. {entry}')
+
+		maximum = self.get_max_pages()
+		if maximum > 1:
+			footer = f'Page {menu.current_page + 1}/{maximum} ({len(self.entries)} entries)'
+			menu.embed.set_footer(text=footer)
+
+		if self.initial_page and self.is_paginating():
+			pages.append('')
+			pages.append('Confused? React with \N{INFORMATION SOURCE} for more info.')
+			self.initial_page = False
+
+		menu.embed.description = '\n'.join(pages)
+		return menu.embed
+
+class SimplePages(_RoboPages):
+	"""A simple pagination session reminiscent of the old Pages interface.
+
+	Basically an embed with some normal formatting.
+	"""
+
+	def __init__(self, entries, *, per_page=12, color=None):
+		super().__init__(SimplePageSource(entries, per_page=per_page))
+		if color == None:
+			color = disnake.Color.blurple()
+		self.embed = disnake.Embed(colour=color)
+
+
+
+
+class CustomRobo(menus.MenuPages):
+	def __init__(self, source):
+		super().__init__(source=source, check_embeds=True)
+
+	async def finalize(self, timed_out):
+		try:
+			if timed_out:
+				await self.message.clear_reactions()
+			else:
+				await self.message.delete()
+		except disnake.HTTPException:
+			pass
+
+class NewHelpMenus(menus.ListPageSource):
+	def __init__(self, entries, *, per_page=12):
+		super().__init__(entries, per_page=per_page)
+		
+	async def format_page(self, menu, entries):
+		pages = []
+		for index, entry in enumerate(entries, start=menu.current_page * self.per_page):
+			pages.append(f'{entry}')
+		
+		maximum = self.get_max_pages()
+		if maximum > 1:
+			footer = f'Page {menu.current_page + 1}/{maximum} ({len(self.entries)} commands)'
+			menu.embed.set_footer(text=footer)
+	
+		menu.embed.description = "\n".join(pages)
+		return menu.embed
+
+class NewCustomMenus(menus.ListPageSource):
+	def __init__(self, entries, *, per_page=12):
+		super().__init__(entries, per_page=per_page)
+		
+	async def format_page(self, menu, entries):
+		pages = []
+		for index, entry in enumerate(entries, start=menu.current_page * self.per_page):
+			pages.append(f'`{index + 1}.` {entry}')
+
+		maximum = self.get_max_pages()
+		if maximum > 1:
+			footer = f'Page {menu.current_page + 1}/{maximum} ({len(self.entries)} entries)'
+			menu.embed.set_footer(text=footer)
+	
+		menu.embed.description = "\n".join(pages)
+		return menu.embed
+
+class HelpmMenu(_RoboPages):
+	def __init__(self, entries, *, per_page=12, title="", color=None):
+		super().__init__(NewHelpMenus(entries, per_page=per_page))
+		if color == None:
+			color = disnake.Color.blurple()
+		self.embed = disnake.Embed(colour=color, title=title)
+
+class CustomMenu(_RoboPages):
+	def __init__(self, entries, *, per_page=12, title="", color=None):
+		super().__init__(NewCustomMenus(entries, per_page=per_page))
+		if color == None:
+			color = disnake.Color.blurple()
+		self.embed = disnake.Embed(colour=color, title=title)
+
+class ToDoMenu(_RoboPages):
+	def __init__(self, entries, *, per_page=12, title="", color=None, author_name=None, author_icon_url=None):
+		super().__init__(NewHelpMenus(entries, per_page=per_page))
+		if color == None:
+			color = disnake.Color.blurple()
+		self.embed = disnake.Embed(colour=color, title=title)
+		if author_name != None:
+			self.embed.set_author(name=author_name, url=author_icon_url, icon_url=author_icon_url)
