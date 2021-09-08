d80cc040dca9199d2358b36192fd50234f115479
Kraots <nagit.robermessi@gmail.com>
2021-09-08 13:52:52 +0300

    last fix

--- cogs/eco.py
+++ cogs/eco.py
@@ -794,9 +794,7 @@ class Economy(commands.Cog):
 		index = 1
 		for m in results:
 			user = self.bot.get_user(m['_id'])
-			if not len(str(int(m['wallet']))) <= 3:
-				em.add_field(name=f"`#{index}` {user.name}", value="{} <:carrots:822122757654577183> ".format(format_balance(m['wallet'])), inline=False)
-			elif not str(m['wallet']).endswith('.0'):
+			if not str(m['wallet']).startswith('0'):
 				em.add_field(name=f"`#{index}` {user.name}", value="{} <:carrots:822122757654577183> ".format(format_balance(m['wallet'])), inline=False)
 			if index == 10:
 				break
