d94e56e70aef7f0d29cc60aa0cc7eff39cf5250f
Kraots <70799557+Kraots@users.noreply.github.com>
2021-09-09 00:57:35 +0300

    Made all the reaction based commands work with the new buttons

--- cogs/todo.py
+++ cogs/todo.py
@@ -12,9 +12,9 @@ class ToDoPageEntry:
 		return f'{self.data}'
 
 class ToDoPages(ToDoMenu):
-	def __init__(self, entries, *, per_page=12, title="", color=color.red, author_name=None, author_icon_url=None):
+	def __init__(self, ctx, entries, *, per_page=12, title="", color=color.red, author_name=None, author_icon_url=None):
 		converted = [ToDoPageEntry(entry) for entry in entries]
-		super().__init__(converted, per_page=per_page, title=title, color=color, author_name=author_name, author_icon_url=author_icon_url)
+		super().__init__(ctx=ctx, entries=converted, per_page=per_page, title=title, color=color, author_name=author_name, author_icon_url=author_icon_url)
 
 class ToDo(commands.Cog):
 
@@ -73,8 +73,8 @@ class ToDo(commands.Cog):
 			index += 1
 			entries[i]['data'] = f"**[{index}.]({entries[i]['url']})** {entries[i]['data']}"
 
-		m = ToDoPages(entries=entries, per_page=10, title="Here's your todo list:", author_name=ctx.author, author_icon_url=ctx.author.avatar.url)
-		await m.start(ctx)
+		m = ToDoPages(ctx=ctx, entries=entries, per_page=10, title="Here's your todo list:", author_name=ctx.author, author_icon_url=ctx.author.avatar.url)
+		await m.start()
 
 	@todo.command(name='delete', aliases=['remove'])
 	async def todo_remove(self, ctx, index):
