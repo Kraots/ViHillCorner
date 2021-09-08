4388b4ef4e9e58a33571756c00ad3d81c06e75ba
Kraots <nagit.robermessi@gmail.com>
2021-09-08 12:52:57 +0300

    Added one more newline in passive's docstring

--- cogs/eco.py
+++ cogs/eco.py
@@ -715,6 +715,7 @@ class Economy(commands.Cog):
 		"""
 		Turn passive `on` or `off`.
 		If passive is on, people won't be able to rob you, but you won't be able to rob anyone as well, additionally, you cannot give or receive carrots from other people.
+		
 		*Note: There's a cooldown of 1 day between each change*
 		"""
 
