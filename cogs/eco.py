5cdf6d802bdff3e71f3e6a9fd094af31fef3f186
Kraots <nagit.robermessi@gmail.com>
2021-09-08 13:49:37 +0300

    This should fix it

--- cogs/eco.py
+++ cogs/eco.py
@@ -794,7 +794,9 @@ class Economy(commands.Cog):
 		index = 1
 		for m in results:
 			user = self.bot.get_user(m['_id'])
-			if not len(str(int(m['wallet']))) <= 3 or not str(m['wallet']).endswith('.0'):
+			if not len(str(int(m['wallet']))) <= 3:
+				em.add_field(name=f"`#{index}` {user.name}", value="{} <:carrots:822122757654577183> ".format(format_balance(m['wallet'])), inline=False)
+			elif not str(m['wallet']).endswith('.0'):
 				em.add_field(name=f"`#{index}` {user.name}", value="{} <:carrots:822122757654577183> ".format(format_balance(m['wallet'])), inline=False)
 			if index == 10:
 				break
