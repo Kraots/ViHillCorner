70018a08ec467b8ca30700d7c6ec2027c6dac304
Kraots <nagit.robermessi@gmail.com>
2021-09-24 23:40:56 +0300

    !calc uses buttons

--- cogs/misc.py
+++ cogs/misc.py
@@ -1,7 +1,6 @@
 from disnake.ext import commands
 import disnake
 import sys
-import async_tio
 import async_cse as cse
 import utils.colors as color
 from utils.helpers import package_version, profile
@@ -39,6 +38,142 @@ class SpotifyView(disnake.ui.View):
 	async def on_timeout(self):
 		await self.message.edit(view=None)
 
+class CalculatorView(disnake.ui.View):
+	def __init__(self, ctx, *, timeout = 180.0):
+		super().__init__(timeout=timeout)
+		self.ctx = ctx
+		self.to_calc = ''
+	
+	async def interaction_check(self, interaction: disnake.MessageInteraction):
+		if interaction.author.id != self.ctx.author.id:
+			await interaction.response.send_message(f'Only {self.ctx.author.display_name} can use this calculator! If you wish to use it too please type `!calc`', ephemeral=True)
+			return False
+		return True
+
+	async def on_error(self, error, item, interaction):
+		if (
+			isinstance(error, SyntaxError) or
+			isinstance(error, disnake.HTTPException)
+		):
+			self.to_calc = ''
+			return await self.update_message()
+		return await self.ctx.bot.reraise(self.ctx, error)
+
+	async def on_timeout(self):
+		for item in self.children:
+			item.disabled = True
+			item.style = disnake.ButtonStyle.gray
+		if len(self.to_calc) != 0:
+			return await self.message.edit(content='Timed Out.', embed=disnake.Embed(description=f'```py\n{eval(self.to_calc)}\n```', color=color.inviscolor), view=self)
+		else:
+			return await self.message.edit(embed=disnake.Embed(description=f'```\nTimed Out.\n```', color=color.inviscolor), view=self)
+
+	async def update_message(self):
+		if len(self.to_calc) != 0:
+			return await self.message.edit(embed=disnake.Embed(description=f'```py\n{self.to_calc}\n```', color=color.inviscolor))
+		await self.message.edit(embed=disnake.Embed(description=f'```py\n{0}\n```', color=color.inviscolor))
+
+	@disnake.ui.button(label='1')
+	async def _1(self, button: disnake.Button, inter: disnake.MessageInteraction):
+		self.to_calc += button.label
+		await self.update_message()
+
+	@disnake.ui.button(label='2')
+	async def _2(self, button: disnake.Button, inter: disnake.MessageInteraction):
+		self.to_calc += button.label
+		await self.update_message()
+
+	@disnake.ui.button(label='3')
+	async def _3(self, button: disnake.Button, inter: disnake.MessageInteraction):
+		self.to_calc += button.label
+		await self.update_message()
+
+	@disnake.ui.button(label='×', style=disnake.ButtonStyle.blurple)
+	async def _multiply(self, button: disnake.Button, inter: disnake.MessageInteraction):
+		self.to_calc += button.label
+		await self.update_message()
+
+	@disnake.ui.button(label='Exit', style=disnake.ButtonStyle.red)
+	async def _exit(self, button: disnake.Button, inter: disnake.MessageInteraction):
+		await self.message.edit(embed=None, view=None, content=f'Quit the calculator session. {self.ctx.author.mention}')
+		self.stop()
+
+	@disnake.ui.button(label='4')
+	async def _4(self, button: disnake.Button, inter: disnake.MessageInteraction):
+		self.to_calc += button.label
+		await self.update_message()
+
+	@disnake.ui.button(label='5')
+	async def _5(self, button: disnake.Button, inter: disnake.MessageInteraction):
+		self.to_calc += button.label
+		await self.update_message()
+
+	@disnake.ui.button(label='6')
+	async def _6(self, button: disnake.Button, inter: disnake.MessageInteraction):
+		self.to_calc += button.label
+		await self.update_message()
+
+	@disnake.ui.button(label='÷', style=disnake.ButtonStyle.blurple)
+	async def _divide(self, button: disnake.Button, inter: disnake.MessageInteraction):
+		self.to_calc += button.label
+		await self.update_message()
+
+	@disnake.ui.button(label='←', style=disnake.ButtonStyle.red)
+	async def _remove_last(self, button: disnake.Button, inter: disnake.MessageInteraction):
+		self.to_calc = self.to_calc[:-1]
+		await self.update_message()
+
+	@disnake.ui.button(label='7')
+	async def _7(self, button: disnake.Button, inter: disnake.MessageInteraction):
+		self.to_calc += button.label
+		await self.update_message()
+
+	@disnake.ui.button(label='8')
+	async def _8(self, button: disnake.Button, inter: disnake.MessageInteraction):
+		self.to_calc += button.label
+		await self.update_message()
+
+	@disnake.ui.button(label='9')
+	async def _9(self, button: disnake.Button, inter: disnake.MessageInteraction):
+		self.to_calc += button.label
+		await self.update_message()
+	
+	@disnake.ui.button(label='+', style=disnake.ButtonStyle.blurple)
+	async def _add(self, button: disnake.Button, inter: disnake.MessageInteraction):
+		self.to_calc += button.label
+		await self.update_message()
+	
+	@disnake.ui.button(label='Clear', style=disnake.ButtonStyle.red)
+	async def _clear(self, button: disnake.Button, inter: disnake.MessageInteraction):
+		self.to_calc = ''
+		await self.update_message()
+
+	@disnake.ui.button(label='00')
+	async def _00(self, button: disnake.Button, inter: disnake.MessageInteraction):
+		self.to_calc += button.label
+		await self.update_message()
+
+	@disnake.ui.button(label='0')
+	async def _0(self, button: disnake.Button, inter: disnake.MessageInteraction):
+		self.to_calc += button.label
+		await self.update_message()
+
+	@disnake.ui.button(label='.')
+	async def _dot(self, button: disnake.Button, inter: disnake.MessageInteraction):
+		self.to_calc += button.label
+		await self.update_message()
+
+	@disnake.ui.button(label='-', style=disnake.ButtonStyle.blurple)
+	async def _substract(self, button: disnake.Button, inter: disnake.MessageInteraction):
+		self.to_calc += button.label
+		await self.update_message()
+
+	@disnake.ui.button(label='=', style=disnake.ButtonStyle.green)
+	async def _result(self, button: disnake.Button, inter: disnake.MessageInteraction):
+		if len(self.to_calc) != 0:
+			res = eval(self.to_calc.replace('÷', '/').replace('×', '*'))
+			await self.message.edit(embed=disnake.Embed(description=f'```py\n{res}\n```', color=color.inviscolor))
+			self.to_calc = str(res)
 
 class UrbanDictionaryPageSource(menus.ListPageSource):
 	BRACKETED = re.compile(r'(\[(.+?)\])')
@@ -272,34 +407,17 @@ class Misc(commands.Cog):
 		view.message = await ctx.send(embed=botinfo, view=view)
 
 	@commands.command(aliases=['calculator', 'calculate'])
-	async def calc(self, ctx, *, operation:str):
+	async def calc(self, ctx):
 		"""Do some basic mathematics operations."""
 		
-		args = operation
-		if '^' in args:
-			args = args.replace('^', '**')
-	
-		tio = await async_tio.Tio()
-		result = await tio.execute(f"print({args})", language="python3")
-		await tio.close()
-		exit_status_ = result.exit_status
-		if int(exit_status_) == 1:
-			return await ctx.reply("Are you sure you're using this command for a valid operation?")
-
-		try:
-			em = disnake.Embed()
-			em.set_author(name=f"Here's your result `{ctx.author.display_name}`:", icon_url=ctx.author.display_avatar)
-			em.add_field(name="Operation:", value=f"`{args}`", inline=False)
-			em.add_field(name="Result:", value=f"`{result.stdout}`")
-			em.color = color.lightpink
-			await ctx.send(embed=em, reference=ctx.replied_reference)
-		except disnake.HTTPException:
-			em = disnake.Embed()
-			em.set_author(name=f"Here's your result `{ctx.author.display_name}`:", icon_url=ctx.author.display_avatar)
-			em.add_field(name="Operation:", value="`Operation too long`", inline=False)
-			em.add_field(name="Result:", value=f"`{result.stdout}`")
-			em.color = color.lightpink
-			await ctx.send(embed=em, reference=ctx.replied_reference)
+		view = CalculatorView(ctx)
+		view.message = await ctx.send(
+			embed=disnake.Embed(
+				description=f'```py\n0\n```',
+				color=color.inviscolor
+			),
+			view=view
+		)
 
 	@commands.command()
 	async def google(self, ctx, *, query):
