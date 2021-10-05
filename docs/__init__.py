2dccb42b1020dc5407229678bffa2ff4c40a733f
Kraots <nagit.robermessi@gmail.com>
2021-10-05 23:56:01 +0300

    Removed the advice because it works either way

--- docs/__init__.py
+++ docs/__init__.py
@@ -14,13 +14,3 @@ def setup(bot: ViHillCorner) -> None:
     """Load the Doc cog."""
     from ._cog import DocCog
     bot.add_cog(DocCog(bot))
-    # There's a chance that it won't add the cog for whatever reason
-    # so it's recommended you also add it in on_ready
-    # Example:
-
-    """in your on_ready:
-    try:
-        bot.load_extension('cogs')
-    except Exception:
-        pass
-    """
