9daeff957a496b6bf8e77e7f0ca41b12f167b853
Kraots <nagit.robermessi@gmail.com>
2021-10-16 23:08:48 +0300

    Added typehinting to "DocItem"

--- docs/cache.py
+++ docs/cache.py
@@ -1,6 +1,11 @@
-from typing import Optional, Union, Dict
+from __future__ import annotations
+
+from typing import Optional, Union, Dict, TYPE_CHECKING
 
 from main import database3
+
+if TYPE_CHECKING:
+    from .cog import DocItem
 
 
 class DocCache:
@@ -8,14 +13,14 @@ class DocCache:
         self._cache = database3['DocsCache']
         self.cache = {}
 
-    def _add_to_local_cache(self, item, value: str) -> None:
+    def _add_to_local_cache(self, item: DocItem, value: str) -> None:
         """Adds to the local cache `self.cache`."""
         k = self.cache.get(item.package)
         if k is None:
             self.cache[item.package] = {}
         self.cache[item.package][item.symbol_id] = value
 
-    async def set(self, item, value: str) -> None:
+    async def set(self, item: DocItem, value: str) -> None:
         """
         Set the Markdown `value` for the symbol `item`.
         All keys from a single page are stored together.
@@ -30,7 +35,7 @@ class DocCache:
         k[item.symbol_id] = value
         await self._cache.update_one({'_id': item.package}, {'$set': {'data': k}})
 
-    async def get(self, item) -> Optional[str]:
+    async def get(self, item: DocItem) -> Optional[str]:
         """Return the Markdown content of the symbol `item` if it exists."""
 
         key: Union[Dict, None] = self.cache.get(item.package)
