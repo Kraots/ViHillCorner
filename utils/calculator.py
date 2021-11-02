e311f6535e97b74c14903e69249f803478e1df80
Kraots <nagit.robermessi@gmail.com>
2021-11-02 21:29:11 +0200

    Fixed calculator saying "This interaction failed"

--- utils/calculator.py
+++ utils/calculator.py
@@ -59,102 +59,125 @@ class Calculator(View):
 
     @disnake.ui.button(label='1')
     async def _1(self, button: disnake.Button, inter: disnake.MessageInteraction):
+        await inter.response.defer()
         self.to_calc += button.label
         await self.update_message()
 
     @disnake.ui.button(label='2')
     async def _2(self, button: disnake.Button, inter: disnake.MessageInteraction):
+        await inter.response.defer()
         self.to_calc += button.label
         await self.update_message()
 
     @disnake.ui.button(label='3')
     async def _3(self, button: disnake.Button, inter: disnake.MessageInteraction):
+        await inter.response.defer()
         self.to_calc += button.label
         await self.update_message()
 
     @disnake.ui.button(label='×', style=disnake.ButtonStyle.blurple)
     async def _multiply(self, button: disnake.Button, inter: disnake.MessageInteraction):
+        await inter.response.defer()
         self.to_calc += button.label
         await self.update_message()
 
     @disnake.ui.button(label='Exit', style=disnake.ButtonStyle.red)
     async def _exit(self, button: disnake.Button, inter: disnake.MessageInteraction):
+        await inter.response.defer()
         await self.message.edit(embed=None, view=None, content=f'Quit the calculator session. {self.ctx.author.mention}')
         self.stop()
 
     @disnake.ui.button(label='4')
     async def _4(self, button: disnake.Button, inter: disnake.MessageInteraction):
+        await inter.response.defer()
         self.to_calc += button.label
         await self.update_message()
 
     @disnake.ui.button(label='5')
     async def _5(self, button: disnake.Button, inter: disnake.MessageInteraction):
+        await inter.response.defer()
         self.to_calc += button.label
         await self.update_message()
 
     @disnake.ui.button(label='6')
     async def _6(self, button: disnake.Button, inter: disnake.MessageInteraction):
+        await inter.response.defer()
         self.to_calc += button.label
         await self.update_message()
 
     @disnake.ui.button(label='÷', style=disnake.ButtonStyle.blurple)
     async def _divide(self, button: disnake.Button, inter: disnake.MessageInteraction):
+        await inter.response.defer()
         self.to_calc += button.label
         await self.update_message()
 
     @disnake.ui.button(label='←', style=disnake.ButtonStyle.red)
     async def _remove_last(self, button: disnake.Button, inter: disnake.MessageInteraction):
+        await inter.response.defer()
         self.to_calc = self.to_calc[:-1]
         await self.update_message()
 
     @disnake.ui.button(label='7')
     async def _7(self, button: disnake.Button, inter: disnake.MessageInteraction):
+        await inter.response.defer()
         self.to_calc += button.label
         await self.update_message()
 
     @disnake.ui.button(label='8')
     async def _8(self, button: disnake.Button, inter: disnake.MessageInteraction):
+        await inter.response.defer()
         self.to_calc += button.label
         await self.update_message()
 
     @disnake.ui.button(label='9')
     async def _9(self, button: disnake.Button, inter: disnake.MessageInteraction):
+        await inter.response.defer()
         self.to_calc += button.label
         await self.update_message()
 
     @disnake.ui.button(label='+', style=disnake.ButtonStyle.blurple)
     async def _add(self, button: disnake.Button, inter: disnake.MessageInteraction):
+        await inter.response.defer()
         self.to_calc += button.label
         await self.update_message()
 
     @disnake.ui.button(label='Clear', style=disnake.ButtonStyle.red)
     async def _clear(self, button: disnake.Button, inter: disnake.MessageInteraction):
+        await inter.response.defer()
         self.to_calc = ''
         await self.update_message()
 
     @disnake.ui.button(label='00')
     async def _00(self, button: disnake.Button, inter: disnake.MessageInteraction):
+        await inter.response.defer()
         self.to_calc += button.label
         await self.update_message()
 
     @disnake.ui.button(label='0')
     async def _0(self, button: disnake.Button, inter: disnake.MessageInteraction):
+        await inter.response.defer()
         self.to_calc += button.label
         await self.update_message()
 
     @disnake.ui.button(label='.')
     async def _dot(self, button: disnake.Button, inter: disnake.MessageInteraction):
+        await inter.response.defer()
         self.to_calc += button.label
         await self.update_message()
 
     @disnake.ui.button(label='-', style=disnake.ButtonStyle.blurple)
     async def _substract(self, button: disnake.Button, inter: disnake.MessageInteraction):
+        await inter.response.defer()
         self.to_calc += button.label
         await self.update_message()
 
     @disnake.ui.button(label='=', style=disnake.ButtonStyle.green)
     async def _result(self, button: disnake.Button, inter: disnake.MessageInteraction):
+        await inter.response.defer()
         if len(self.to_calc) != 0:
-            res = eval(self.to_calc.replace('÷', '/').replace('×', '*'))
+            try:
+                res = eval(self.to_calc.replace('÷', '/').replace('×', '*'))
+            except Exception:
+                res = 'Error.'
             await self.message.edit(embed=disnake.Embed(description=f'```py\n{res}\n```', color=Colours.invisible))
             self.to_calc = str(res)
