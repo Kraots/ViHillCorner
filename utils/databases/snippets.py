3bfc291f5cbcaca79b6670ec8f3b04386792000e
Kraots <70799557+Kraots@users.noreply.github.com>
2021-11-15 10:23:45 +0200

    Fix wrong field type

--- utils/databases/snippets.py
+++ utils/databases/snippets.py
@@ -9,7 +9,7 @@ instance = Instance(database1)
 
 @instance.register
 class Snippet(Document):
-    name = IntField(attribute='_id', required=True)
+    name = StrField(attribute='_id', required=True)
     content = StrField(required=True)
     owner_id = IntField(required=True)
     uses_count = IntField(required=True)
